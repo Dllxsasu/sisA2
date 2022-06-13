feaf0f7fa82fbbca009f420b0e398d71f742c89e
Dllxsasu <43934979+Dllxsasu@users.noreply.github.com>
2022-06-12 22:21:15 -0500

    looc

--- ingPSistemas-actividad2/src/controlador/EmpleadoControlador.java
+++ ingPSistemas-actividad2/src/controlador/EmpleadoControlador.java
@@ -11,6 +11,9 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import Modelo.Empleado;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author sasu
@@ -44,10 +47,45 @@ public class EmpleadoControlador {
            System.out.println("Error: " + ex);
             return false;
         }
-    //    return false;
-        
+    //    return false;       
     }
-    
+     public DefaultTableModel ListarEmpleados() {
+        PreparedStatement ps = null;
+        Connection conn = Conexion.getConnection();
+        ResultSet rs = null;
+     
+        String[] titulos = {"Codigo", "DNI", "nombres" , "Ape Paterno", "Ape Materno", "Genero", "Area", "F. Nacimiento", "F. ingreso", "Salario"};
+        Object[] registro = new Object[10];
+        DefaultTableModel modelo = new DefaultTableModel(null, titulos);
+        String cSQL = "SELECT `codigo`, `DNI`, `nombre`, `apellidoPaterno`, `apellidoMaterno`, `genero`, `fechaNacimiento`, `fechaIngresoEmpresa`, `salario` FROM `empleado`";
+
+        try {
+            ps = conn.prepareStatement(cSQL);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                registro[0] = rs.getString(1);
+                registro[1] = rs.getString(2);
+                registro[2] = rs.getString(3);
+                registro[3] = rs.getString(4);
+                registro[4] = rs.getString(5);
+                registro[5] = rs.getString(6);
+                registro[6] = rs.getString(7);
+                registro[7] = rs.getString(8);
+                registro[8] = rs.getString(9);
+               
+
+                modelo.addRow(registro);
+            }
+
+            return modelo;
+
+        } catch (SQLException e) {
+            JOptionPane.showConfirmDialog(null, e + " error mostrar listar Estacion");
+            return null;
+        }
+    }
+     
     public boolean Modificar(){
         
         
